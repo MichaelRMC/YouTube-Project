35332442756a27ea2e68cf5e9caf77dacea7aa0f
DaviiJones <davonbridgett@pursuit.org>
2023-08-26 13:30:59 -0400

    added connectivity, added bootstrap cards to Aboutus.jsx

--- clone/src/App.jsx
+++ clone/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Aboutus from './components/Aboutus'
+import Navbar from './components/Navbar'
 import './App.css'
 
 function App() {
