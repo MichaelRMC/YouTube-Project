0a4a1c3fc5bb68d559c8ed5da941c9cddb8442c9
Michael Caldwell <michaelcaldwell@pursuit.org>
2023-08-26 14:48:31 -0400

    Add SearchBar Component

---
+++ clone/src/components/SearchBar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useState, useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+
+
+
+ function SearchBar() {
+	const [searchParams, setSearchParams] = useSearchParams()
+
+	function handleSubmit(event) {
+		event.preventDefault();
+		setSearchParams(searchParams.get(event.target))
+	}
+
+  return (
+	<div>
+		<form onSubmit={handleSubmit}>
+			<input type="search" name="search-bar" id="search-bar" placeholder="Search" />
+			<button type="submit">Submit</button>
+		</form>
+	</div>
+  )
+}
+
+export default SearchBar
