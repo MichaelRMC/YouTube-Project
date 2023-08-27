e584a2c0cafde80927df68cf1e830bf01bb8c106
Anthony Huarneck <anthonyhuarneck@anthonys-air.myfiosgateway.com>
2023-08-27 09:52:41 -0400

    Creates Home. Sets up basic Router structure.

--- clone/src/App.jsx
+++ clone/src/App.jsx
@@ -1,31 +1,25 @@
-import { useState } from 'react'
-
+import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-
-import Aboutus from './components/Aboutus'
-import Navbar from './components/Navbar'
-
-import './App.css'
-import {  getVideo } from './api/fetch'
+import Home from "./components/Home";
+import Aboutus from "./components/Aboutus";
+import Navbar from "./components/Navbar";
+import "./App.css";
+import { getVideo } from "./api/fetch";
 
 function App() {
 
   // console.log(getVideo())
 
-
-
   return (
-
-    <div className='wrapper'>
-
-
+    <div className="wrapper">
       <Router>
-
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </Router>
     </div>
-
-  )
+  );
 }
 
-export default App
+export default App;
--- clone/src/api/fetch.js
+++ clone/src/api/fetch.js
@@ -4,7 +4,7 @@ let search = "dogs";
 
 export function getVideo() {
   return fetch(
-    `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=8&q=ariana%20grande&key=${URL}`
+    `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=8&q=${search}&key=${URL}`
   )
     .then((response) => {
       response.json();
