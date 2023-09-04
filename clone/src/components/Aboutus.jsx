c2c75250c770c7ae6388025ca9d6d7d30c9eaf81
DaviiJones <davonbridgett@pursuit.org>
2023-09-04 14:53:11 -0400

    about us page done

--- clone/src/components/Aboutus.jsx
+++ clone/src/components/Aboutus.jsx
@@ -1,42 +1,51 @@
 import React from "react";
 import Navbar from "./Navbar";
 
-
 export default function Aboutus() {
     return (
         <>
-        <Navbar />
-        <div>
-            <div className="card" style={{ width: '18rem' }}>
-                <div className="card-body">
-                    <h5 className="name">Anthony Huarneck</h5>
-                    <h6 className="dateOfBirth">March 20, 1994</h6>
-                    <p className="hobbies"></p>
-                    <a href="#" className="card-link">https://github.com/anthuarneck</a>
-                    {/* Codetrack links? */}
-                    <a href="#" className="card-link">Another link</a>
-
+            <Navbar />
+            <div>
+                <div className="card" style={{ width: "18rem" }}>
+                    <div className="card-body">
+                        <img href="https://avatars.githubusercontent.com/u/131826343?v=4"></img>
+                        <h5 className="name">Anthony Huarneck</h5>
+                        <h6 className="dateOfBirth">March 20, 1994</h6>
+                        <a href="#" className="card-link">
+                            https://github.com/anthuarneck
+                        </a>
+                        <a href="#" className="https://pursuit.codetrack.dev/fellow/917">
+                            CodeTrack
+                        </a>
+                    </div>
+                </div>
+                <div className="card" style={{ width: "18rem" }}>
+                    <div className="card-body">
+                        <img href="https://avatars.githubusercontent.com/u/129802219?v=4"></img>
+                        <h5 className="name">Davon Bridgett</h5>
+                        <h6 className="dateOfBirth">March 13, 1993</h6>
+                        <a href="#" className="card-link">
+                            https://github.com/DaviiJones/YouTube-Project
+                        </a>
+                        <a href="#" className="https://pursuit.codetrack.dev/profile">
+                            CodeTrack
+                        </a>
+                    </div>
+                </div>
+                <div className="card" style={{ width: "18rem" }}>
+                    <div className="card-body">
+                        <img href="https://avatars.githubusercontent.com/u/127798396?v=4"></img>
+                        <h5 className="name">Michael Caldwell</h5>
+                        <h6 className="dateOfBirth">May 3rd 1984</h6>
+                        <a href="#" className="card-link">
+                            https://github.com/MichaelRMC
+                        </a>
+                        <a href="#" className="https://pursuit.codetrack.dev/fellow/926">
+                            CodeTrack
+                        </a>
+                    </div>
                 </div>
             </div>
-            <div className="card" style={{ width: '18rem' }}>
-                <div className="card-body">
-                    <h5 className="name">Davon Bridgett</h5>
-                    <h6 className="dateOfBirth">March 13, 1993</h6>
-                    <p className="hobbies"></p>
-                    <a href="#" className="card-link">https://github.com/DaviiJones/YouTube-Project</a>
-                    <a href="#" className="card-link">Another link</a>
-                </div>
-            </div>
-            <div className="card" style={{ width: '18rem' }}>
-                <div className="card-body">
-                    <h5 className="name">Michael Caldwell</h5>
-                    <h6 className="dateOfBirth">May 3rd 1984</h6>
-                    <p className="hobbies"></p>
-                    <a href="#" className="card-link">https://github.com/MichaelRMC</a>
-                    <a href="#" className="card-link">Another link</a>
-                </div>
-            </div>
-        </div>
         </>
     );
 }
