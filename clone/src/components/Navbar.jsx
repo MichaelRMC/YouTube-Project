5118e4a3db446d7aeb943b9b7da9f2e11b581c6e
DaviiJones <davonbridgett@pursuit.org>
2023-09-04 22:06:36 -0400

    basic .css for home

--- clone/src/components/Navbar.jsx
+++ clone/src/components/Navbar.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom"
+import "./Navbar.css"
+
 
 export default function Navbar() {
 
     return (
         <div className="navBar">
             <header>
+            <img src="https://cdn-icons-png.flaticon.com/256/152/152810.png"></img>
             <Link to="/"><h2>Home</h2></Link>
-            <Link to="/about"><h2>About</h2></Link>
+            <Link to="/about"><h2>About Us!</h2></Link>
             </header>
         </div>
     )
